425ed8e830e2f32627837284896c668c01b57032
akashfd <ahsanshamim193@gmail.com>
2024-04-21 16:20:57 +0600

    varriable operator practice

---
+++ varriable.js
@@ -0,0 +1,36 @@
+// example var
+var shamim= 100;
+var pondit= 130;
+console.log(pondit)
+
+
+
+// example let
+let akash = 120;
+ pondit = 130;
+ console.log(pondit)
+
+
+
+//  example const 
+ const ahsan = 120;
+pondit = 130;
+ console.log(pondit)
+
+
+
+
+//  addition 
+let x = 12
+let y= 13
+let xy = x+y
+console.log(xy);
+
+// division
+var a = 65
+var b= 45
+result= a-b
+console.log(result)
+
+
+
